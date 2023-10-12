5147544b1683e958f3c292f8a359f9b859d8eef9
Kyle Coble <kwc57@cornell.edu>
2023-10-12 18:49:17 -0500

    Formatting

--- src/generic_stream.py
+++ src/generic_stream.py
@@ -1,29 +1,38 @@
 #!/usr/bin/env python3
+# Copyright (c) farm-ng, inc.
+#
+# Licensed under the Amiga Development Kit License (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://github.com/farm-ng/amiga-dev-kit/blob/main/LICENSE
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 from __future__ import annotations
+
 import asyncio
-import argparse
 from pathlib import Path
-from std_msgs.msg import String
 
 import rospy
-
 from farm_ng.core.event_client import EventClient
+from farm_ng.core.event_service_pb2 import EventServiceConfigList
+from farm_ng.core.event_service_pb2 import SubscribeRequest
 from farm_ng.core.events_file_reader import proto_from_json_file
-from farm_ng.core.event_service_pb2 import (
-    EventServiceConfig,
-    EventServiceConfigList,
-    SubscribeRequest,
-)
+from std_msgs.msg import String
 
 
 def _farmng_to_ros_type(farmng_type):
-    """Map the farmng type to the ros type"""
+    """Map the farmng type to the ros type."""
     # TODO: implement me
     return String
 
 
 def _farmng_to_ros(farmng_msg):
-    """Convert the farmng message to the ros message"""
+    """Convert the farmng message to the ros message."""
     # TODO: implement me
     return str(farmng_msg)
 
@@ -44,14 +53,16 @@ async def subscribe(service_name, clients):
 
     subscribe_tasks = []
     for uri in uris:
-        if not uri.path in publishers:
+        if uri.path not in publishers:
             topic = f"/{service_name}{uri.path}"
             print(f"Creating ROS publisher for: {topic}")
             ros_msg_type = _farmng_to_ros_type(uri.path)
             publisher = rospy.Publisher(topic, ros_msg_type, queue_size=10)
 
         subscribe_request = SubscribeRequest(uri=uri)
-        subscribe_tasks.append(asyncio.create_task(_subscribe(client, subscribe_request, publisher)))
+        subscribe_tasks.append(
+            asyncio.create_task(_subscribe(client, subscribe_request, publisher))
+        )
     return subscribe_tasks
 
 
@@ -68,30 +79,32 @@ async def run(service_config: Path) -> None:
     for config in config_list.configs:
         if config.port != 0:
             clients[config.name] = EventClient(config)
-    
+
     for service_name in clients.keys():
         service_tasks = await subscribe(service_name, clients)
         tasks.extend(service_tasks)
 
-
     await asyncio.gather(*tasks)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # TODO: Get the arg as required from the roslaunch file
     # parser = argparse.ArgumentParser(description='Amiga ROS Bridge')
     # parser.add_argument('--service-config', type=Path, required=True, help='Path to config file')
     # args = parser.parse_args()
 
     # HACK: Force the config we know is there
-    service_config = Path(__file__).resolve().parent.parent / "include" / "service_config_canbus.json"
+    service_config = (
+        Path(__file__).resolve().parent.parent
+        / "include"
+        / "service_config_canbus.json"
+    )
 
     try:
         # start the ros node
         loop = asyncio.get_event_loop()
-        rospy.init_node('my_tutorial_node')
+        rospy.init_node("my_tutorial_node")
         rospy.loginfo("my_tutorial_node started!")
         loop.run_until_complete(run(service_config))
     except rospy.ROSInterruptException:
         pass
-
